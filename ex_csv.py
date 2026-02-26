4e6471442475789235319752e2cb460c862c1ce8
Felipe M. <felipe.moura0910@gmail.com>
2026-02-26 09:05:07 -0300

    commit aula 3

--- ex_csv.py
+++ ex_csv.py
@@ -1,10 +1,8 @@
 # LISTA DE EXERCÍCIOS – ANÁLISE DE DADOS COM PANDAS Dataset: Ranking
 # Mundial de Universidades (notas.csv)
 
-
-
 import pandas as pd
-df = pd.read_csv("C:\\Users\\Felipe M\\OneDrive\\Faculdade\\Programação Estruturada\\Estruturada\\notas_base.csv")
+df = pd.read_csv(r"C:\\Users\\Felipe M\\OneDrive\\Faculdade\\Programação Estruturada\\Analise_de_Dados\\notas_base.csv")
 df.shape
 df.columns
 df.dtypes
@@ -12,42 +10,70 @@ df.isna().sum()
 df.head()
 df.tail()
 
-
-
 # ============================================================
 # EXPLORAÇÃO INICIAL (EDA BÁSICA)
 # ============================================================
 
 # Exercício 1 – Conhecendo o Dataset 
 # 1. Quantas linhas e colunas existem?
+df.shape
+
 # 2. Quais são os tipos de dados? 
+df.dtypes
 # 3. Existe coluna com valores ausentes?
+df.isnull().sum()
 # 4. Qual é o período de anos disponível? 
-# 5. Quantos países diferentes
-# existem?
+df.columns
+df.loc[:,"year"].unique()
+# 5. Quantos países diferentes existem?
+df.loc[:,"country"].unique()
+
 
 # Exercício 2 – Estatísticas Gerais 
 # 1. Média do score 
+df.loc[:,"score"].mean()
 # 2. Maior score 
+df.loc[:,"score"].max()
 # 3.Menor score 
+df.loc[:,"score"].min()
 # 4. Média do score por ano 
+df.groupby("year")["score"].mean()
 # 5. Desvio padrão do score
+df.loc[:,"score"].std()
 
 # ============================================================
 # FILTROS E SELEÇÕES
 # ============================================================
 
 # Exercício 3 – Top Universidades 
-# 1. Mostre as 10 melhores universidades do mundo (menor world_rank) 
+# 1. Mostre as 10 melhores universidades do mundo (menor world_rank)
+df.loc[:, ["institution", "world_rank"] ] .sort_values("world_rank")
+
 # 2. Mostre as 5 melhores universidades do Brasil (se existirem) 
+filtro = df["country"]=="Brazil"
+df_brazil = df.loc[filtro, ["institution","national_rank", "country"]] .sort_values("national_rank")
+df_brazil.iloc[0:5]
+
 # 3. Mostre universidades com score maior que 90 
+filtro = df["score"]>90
+df.loc[filtro, ["institution", "score", "country"] ] .sort_values("score")
+
 # 4. Mostre universidades dos EUA com score maior que 80
+filtro = (df["score"]>80) & (df["country"]== "USA")
+df.loc[filtro, ["institution", "score", "country"] ] .sort_values("score",ascending=False)
 
 # Exercício 4 – Seleção Avançada 
 # 1. Mostre apenas as colunas: institution,
 # country e score 
+df.loc[:, ["institution", "country", "score"]] .sort_values("score",ascending=False)
+
 # 2. Mostre universidades entre rank 50 e 100 
+filtro = (df["world_rank"]>=50) & (df["world_rank"]<=100)
+df.loc[filtro, ["institution", "world_rank", "country", "year"] ] .sort_values("world_rank")
+
 # 3. Mostre universidades cujo país é “United Kingdom”
+filtro = df["country"]== "United Kingdom"
+df.loc[filtro, ["institution", "country", "national_rank"]] .sort_values("national_rank")
 
 # ============================================================ PARTE 3 –
 # MISSING VALUES
@@ -55,6 +81,7 @@ df.tail()
 
 # Exercício 5 – Valores Ausentes 
 # 1. Quantos valores nulos existem na coluna broad_impact? 
+
 # 2. Qual percentual do dataset é nulo? 
 # 3. Remova linhas com broad_impact nulo 
 # 4. Preencha valores nulos com a média 
